233f519d07ea89f111d75a7f9ceac99b7993ba1d
Theo BRICHET <theo.brichet@etu.univ-nantes.fr>
2019-12-28 16:23:43 +0100

    Update scripts

--- gitCommit.bat
+++ gitCommit.bat
@@ -1,4 +1,5 @@
 git add Engine/*
 git add Game/*
+git add 2DDevelop/*
 git status
 git commit -m %1
--- gitCommitPush.bat
+++ gitCommitPush.bat
@@ -1,5 +1,6 @@
 git add Engine/*
 git add Game/*
+git add 2DDevelop/*
 git status
 git commit -m %1
 git push
