94403dfb03ea85387ff19a8be460daeb08e29c02
Theo Brichet <theo.brichet@etu.univ-nantes.fr>
2020-02-20 18:31:46 +0100

    Change texture format fro the font (Now rendered in white by default)

--- Engine/TextRendering/Font.cpp
+++ Engine/TextRendering/Font.cpp
@@ -11,13 +11,13 @@ namespace Engine
 		, mFont()
 		, mAtlasTexture(nullptr)
 	{
-		mAtlas = texture_atlas_new(512, 512, 1);
+		mAtlas = texture_atlas_new(512, 512, 3);
 		mFont = texture_font_new_from_file(mAtlas, size, path);
 		texture_font_load_glyphs(mFont, " !\"#$%&'()*+,-./0123456789:;<=>?");
 		texture_font_load_glyphs(mFont, "@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_");
 		texture_font_load_glyphs(mFont, "`abcdefghijklmnopqrstuvwxyz{ |}~");
 		mAtlasTexture = CreateRef<OpenGL::Texture2D>(512, 512);
-		mAtlasTexture->SetData(mAtlas->data, GL_RGB, GL_RED);
+		mAtlasTexture->SetData(mAtlas->data, GL_RGB, GL_RGB);
 	}
 
 	Ref<Texture2D> Font::GetTextureAtlas() const
